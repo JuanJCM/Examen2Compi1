ec880db788a54d770bde70d3b478168e61a9b34b
JuanJC199 <juancarranza96@outlook.com>
2021-09-22 21:21:11 -0600

    init

---
+++ DotNetWebTemplateCompiler-master/DotNetWeb.Core/Type.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetWeb.Core
+{
+    public class Type : IEquatable<Type>
+    {
+        public string Lexeme { get; private set; }
+
+        public TokenType TokenType { get; private set; }
+        public Type(string lexeme, TokenType tokenType)
+        {
+            Lexeme = lexeme;
+            TokenType = tokenType;
+        }
+        public bool Equals(Type other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Lexeme == other.Lexeme && TokenType == other.TokenType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            return Equals((Type)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Lexeme, (int)TokenType);
+        }
+
+        public static bool operator ==(Type a, Type b) => a.Equals(b);
+
+        public static bool operator !=(Type a, Type b) => !a.Equals(b);
+
+        public override string ToString()
+        {
+            return Lexeme;
+        }
+    }
+}
